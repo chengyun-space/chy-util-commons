6dc2c34d25620d738ab48b6b909379641a71b2aa
chengyun <3131113645@qq.com>
2022-01-18 09:08:05 +0800

    rfdsfs

---
+++ demo/src/test/java/com/yun/com/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yun.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
